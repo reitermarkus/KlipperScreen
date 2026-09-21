e05b5eaf690e50a6ff43fa7729458a260822145d
Markus Reiter <me@reitermark.us>
2026-09-21 03:29:28 +0200

    Fix args.

--- panels/led.py
+++ panels/led.py
@@ -181,7 +181,8 @@ class Panel(ScreenPanel):
         self.color_data = color_data
         self.update_preview()
 
-    def update_color_data(self, idx):
+    def update_color_data(self, idx, *args):
+        logging.info(f"update_color_data idx = {idx}, args = {args}")
         self.color_data[idx] = self.scales[idx].get_value() / 255
         self.update_preview()
 
